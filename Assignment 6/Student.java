8ce95c09417683691fb3a3429957587daaa8de9a
JoshuaRobinsonJ <97521677+JoshuaRobinsonJ@users.noreply.github.com>
2022-01-24 09:53:23 +0530

    Add files via upload

---
+++ Assignment 6/Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+	String name;
+	int roll_no;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj=new Student();
+		obj.name="Joshua J";
+		obj.roll_no=5;
+		System.out.println("Student Name ="+obj.name);
+		System.out.println("Roll_no. ="+obj.roll_no);
+
+	}
+
+}
